71f5499d5839655350ef5eb9bcdab52f3e583e44
Darshan Dhanvate <darshandhanvate55@gmail.com>
2025-08-15 19:57:52 +0530

    Initial commit with first code file

---
+++ day1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int n= sc.nextInt();
+        int arr[]= new int[n];
+        for( int i=0; i<n; i++){
+            
+        }
+        sc.close();
+    }
+}
